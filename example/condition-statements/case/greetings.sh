8a84eb043c18520220fa662359c81f63b04b9620
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2018-02-21 17:21:43 +0530

    update

---
+++ example/condition-statements/case/greetings.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# take user name
+echo "Enter your name:"
+read name
+
+# take time of the day
+echo "Enter time of the day [Morning/Afternoon/Evening/Night]:"
+read daytime
+
+case $daytime in
+  "Morning")
+    echo "Good Morning $name"
+    ;;
+
+  "Afternoon")
+    echo "Good Afternoon $name"
+    ;;
+
+  "Evening")
+    echo "Good Evening $name"
+    ;;
+    
+  "Night")
+    echo "Good Night $name"
+    ;;
+esac
+
+echo "End of script."
